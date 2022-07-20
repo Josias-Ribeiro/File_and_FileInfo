f244a16e19ca3311c8547cfdc0baaf8ab1fc3a79
Josias-Ribeiro <josias_ribeiro90@hotmail.com>
2022-07-20 14:24:19 -0300

    FFI - Criação da estrutura criando arquivo teste

--- Program.cs
+++ Program.cs
@@ -4,17 +4,40 @@ using static System.Console;
 
 WriteLine("Digite o nome do arquivo");
 var nome = ReadLine();
+nome = LimparNome(nome);
 
 var path = Path.Combine(Environment.CurrentDirectory, $"{nome}.txt");
 
 CriarArquivo(path);
+WriteLine("Digite enter para finalizar");
+ReadLine();
+
+static string LimparNome(string nome)
+{
+    foreach (var caractere in Path.GetInvalidFileNameChars())
+    {
+        nome = nome.Replace(caractere, '-');
+    }
+
+    return nome;
+}
 
 static void CriarArquivo(string path)
 {
-    var sw = File.CreateText(path);
+    try
+    {
+        var sw = File.CreateText(path);
 
-    sw.WriteLine("Primeira linha do arquivo");
-    sw.Flush();
+        sw.WriteLine("Primeira linha do arquivo");
+        sw.Flush();
+    }
+    catch
+    {
+
+        WriteLine("Nome do arquivo inválido");
+    }
+
+
 }
 
 
